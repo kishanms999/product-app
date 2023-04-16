deaaac0def4e6d18a39796596cff31662ed584c0
kishanms999 <kishanms99@gmail.com>
2023-04-16 17:49:10 +0530

    using express midware and app.listen

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-const http=require('http');
-const routes=require('./routes');
-console.log(routes.someText);
-const server=http.createServer(routes.handler)         
-       
-    
-server.listen(3000);
+const express=require('express');
+const app=express();
+app.use((req,res,next)=>{
+    console.log('In the middleware');
+    next();
+});
+app.use((req,res,next)=>{
+    console.log('In another middleware');
+    res.send('<h1>Hello from Express</h1>');
+});
+
+app.listen(3000);
